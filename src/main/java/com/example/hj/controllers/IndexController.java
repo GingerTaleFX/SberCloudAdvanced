154309b649d64d9a2d7caf35754d3ed875f62326
robotizac <robotizac@gmail.com>
2020-11-07 21:32:33 +0300

    initial

---
+++ src/main/java/com/example/hj/controllers/IndexController.java
@@ -0,0 +1,16 @@
+package com.example.hj.controllers;
+
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/")
+public class IndexController {
+
+    @GetMapping
+    public String index(Model model) {
+        return "hi there";
+    }
+}
---
+++ src/test/java/com/example/hj/HjApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.hj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HjApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
